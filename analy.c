f90fc2ef8708265d6f5188a810c25baaeea5d21c
iattach <memoryinwinter@outlook.com>
2021-03-15 17:14:35 +0100

    v1

--- analy.c
+++ analy.c
@@ -7,13 +7,29 @@ struct Token * tokens;
 char * c ;
 int posT;
 int pos ;
-int currentR;
 int verified=0;
+int indexR[100];
+int top=-1;
+int find =0;
+int currentC ;
 
 void nTerminal();
 void terminal();
 void nextrule();
-void rule();
+int rule();
+
+int push(int* indexR,int top_,int elem){
+    indexR[++top_]=elem;
+    return top_;
+}
+
+int pop(int * indexR,int top_){
+    if (top_==-1) {
+        return -1;
+    }
+    top_--;
+    return top_;
+}
 
 void printing(int i){
     
@@ -23,22 +39,22 @@ void printing(int i){
         printf("\n");
         break;
     case 1:
-        printf("%s ",tokens[posT].value);
+        printf(" %s ",tokens[posT].value);
         break; 
     case 2:
-        printf("%s -> ",tokens[posT].value);
+        printf(" %s ->",tokens[posT].value);
         break;
     default:
         break;
     }
 }
 void compareValue(){
-    printf("compareValue\n");
+    //printf("Fonction : compareValue\n");
     int i=0;
     int pass =1;
     
     while(tokens[posT].value[i]!= '\0'&& pass){
-        printf("while %c :%s : %d compare to %c : %s\n",tokens[posT].value[i],tokens[posT].value,i,c[pos],c);
+        printf("    posIndex %d -> %c == %c ? %s\n",pos, c[pos],tokens[posT].value[i],c);
         if(tokens[posT].value[i]==c[pos] ){
             pos++;
         }else{
@@ -46,72 +62,105 @@ void compareValue(){
         }
         i++;
     }
+    
     if(pass){
         posT++;
         terminal();
     }else{
+        printf("    compareValue Not pass\n");
         nextrule();
     }
     
 }
 void terminal(){
-    printf("terminal\n");
+    //printf("Fonction : terminal\n");
+    int ruleT;
     switch(tokens[posT].key){
         case TERMINAL:
             printing(1);
             compareValue();
             break;
         case NTERMINAL:
-            rule();
-            terminal();
+            ruleT=posT; 
+            if(rule()){
+                posT=ruleT+1;
+                terminal();
+            }            
             break;
         case NL:
-            break;
-        default:
-            if(c[pos]=='\0' && tokens[posT+1].key==NL){
+            if(c[pos]=='\0'){
                 verified=1;
             }
+            find = 1;
+            break;
+        default:
+            
             break;
     }
 
 }
 void nTerminal(){
-    printf("nTerminal\n");
+    //printf("Fonction : nTerminal\n");
     switch(tokens[posT].key){
         case TERMINAL:
             break;
         case NTERMINAL:
             printing(2);
-            currentR=posT-1;
+            //currentR=posT-1;
+            currentC=pos;
             posT++;
             terminal();
+            break;
         case NL:
+            //printf("    nTerminal NL\n");
             nextrule();
+            break;
         default:
             break;
     }
 }
 
-void rule(){
-    printf("rule\n");
-    int find =0;
+int rule(){
+    find = 0;
+    printf("{\n");
+    //Fonction : rule\n");
+    
     char * ruleT=tokens[posT].value;
 
     struct Token* p = getTokens();
     for (int i = 0; i < getTokenLen()-1 && !find ; i++){
-        printf("while %s : %d compare to %s,%d,%d\n",p[i+1].value,i,ruleT,p->key,p[i+1].key==NTERMINAL);
-        if(p->key==NL && p[i+1].key==NTERMINAL && strcmp(p[i+1].value, ruleT)==0){
+        find = 0;
+        printf("    rule-> index : %d -> value : %s ->> value(i+1) : %s compare to ruleT :%s \n",i,p[i].value,p[i+1].value,ruleT);
+        if(p[i].key==NL && p[i+1].key==NTERMINAL && strcmp(p[i+1].value, ruleT)==0){
                 ///find=1;
+                //pos=posOfC-1;
+                //pos=currentC;
+                printf("    push pos : %d\n",pos);
+                top=push(indexR,top,pos);
                 posT=i+1;
+                sleep(1);
                 nTerminal();
+                if(!find){
+                    pos=indexR[top];
+                    top=pop(indexR,top);
+                    printf("    pop pos : %d\n",pos);
+                }
+                
         }
-        sleep(1);
-        p++;
+        //sleep(1);
+        //p++;
     }   
+    
+    printf("}\n");//Fonction Fin: rule\n");
+    if(!find){
+        pos=currentC;
+        printf("    current pos : %d \n",pos);
+    }
+    return find;
 }
 void nextrule(){
-    printf("nextrule\n");
-    char * ruleT=tokens[posT].value;
+    printf("Fonction : nextrule\n");
+  //  char * ruleT=tokens[posT].value;
 
 }
 void analy( char * cr){
@@ -123,10 +172,14 @@ void analy( char * cr){
 
     struct Token* p =getTokens();
     for (int i = 0; i <  getTokenLen(); i++){
-        printf("%d: key->%u, value->%s.\n", i, p->key, p->value);
+        //printf("%d: key->%u, value->%s.\n", i, p->key, p->value);
         p++;
     }
-    nTerminal();  
+
+    
+    rule();
+    
+    
     
     if(verified){
         printf("verification passed\n");
--- main.c
+++ main.c
@@ -4,26 +4,11 @@
 int main(int argc , char ** argv){
 
     lexRun();
-
+    char *c = argv[1];
     int len = getTokenLen();
     if (!len) {
         return 0;
     }
-
-    struct Token* p = getTokens();
-    for (int i = 0; i < len; i++){
-        printf("%d: key->%u, value->%s.\n", i, p->key, p->value);
-        p++;
-    }
-
-    char *c = argv[1];
-
-    int i=0;
-    while(c[i]!='\0'){
-        printf("%c\n",c[i]);
-        i++;
-    }
-
     
     analy(c);
     
